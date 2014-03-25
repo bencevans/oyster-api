00b480824a074068ce23bc1feda12d4757eddcd5
Ben Evans <ben@bensbit.co.uk>
2014-03-25 02:13:28 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+var http       = require('http');
+var express    = require('express');
+var poweredBy  = require('poweredby');
+var errorHandler = require('./lib/errorHandler');
+var authOyster = require('./lib/authOyster');
+var app        = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(poweredBy('Slow Crawly Things'));
+app.use(express.logger('dev'));
+app.use(app.router);
+app.use(errorHandler);
+
+app.get('/', function(req, res, next) {
+  res.redirect('https://github.com/bencevans/oyster-api');
+});
+
+app.get('/balance', authOyster, function(req, res) {
+  req.oyster.balance(function(err, balance) {
+    if(err) {
+      return next(err);
+    }
+    res.send({
+      balance: balance
+    });
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Oyster API listening on port ' + app.get('port'));
+});
